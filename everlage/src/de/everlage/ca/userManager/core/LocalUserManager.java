d14783d1bda22fed2b630a2f624e79ba6d633c4e
waffel <waffel>
2003-01-16 13:49:06 +0000

    [feature] Local impelementation of the userManager

---
+++ everlage/src/de/everlage/ca/userManager/core/LocalUserManager.java
@@ -0,0 +1,16 @@
+/**
+ *  File: LocalUserManager.java    
+ * Created on Jan 13, 2003
+ *
+*/
+package de.everlage.ca.userManager.core;
+
+
+/**
+ * New Class
+ * @author waffel
+ *
+ * 
+ */
+public final class LocalUserManager {
+}
