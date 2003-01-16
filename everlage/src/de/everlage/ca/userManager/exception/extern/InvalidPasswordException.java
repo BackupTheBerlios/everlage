66d39f37636c2fc0e394ef10058e82a099e1a051
waffel <waffel>
2003-01-16 13:17:07 +0000

    [feature] Exception thrown when invalid password detected

---
+++ everlage/src/de/everlage/ca/userManager/exception/extern/InvalidPasswordException.java
@@ -0,0 +1,18 @@
+/**
+ * $ID$
+ * File:   InvalidPasswordException.java    Created on Jan 10, 2003
+ *
+*/
+package de.everlage.ca.userManager.exception.extern;
+
+import java.io.Serializable;
+
+/**
+ * New Class
+ * @author waffel
+ *
+ * 
+ */
+public class InvalidPasswordException extends Exception implements Serializable {
+
+}
