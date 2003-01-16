5dcb14c6233ad2c19e724bf2cb82a3e41158679e
waffel <waffel>
2003-01-16 13:15:51 +0000

    [feature] Communiaction Object wich represents an User

---
+++ everlage/src/de/everlage/ca/userManager/comm/extern/UserData.java
@@ -0,0 +1,20 @@
+/**
+ *  File: UserData.java    
+ * Created on Jan 10, 2003
+ *
+*/
+/* $ID$ */
+
+package de.everlage.ca.userManager.comm.extern;
+
+import java.io.Serializable;
+
+/**
+ * New Class
+ * @author waffel
+ *
+ * 
+ */
+public final class UserData implements Serializable {
+
+}
