df8d93a75979bfca669b4f4c6e388af592e3a789
waffel <waffel>
2003-01-16 13:02:24 +0000

    [feature] Global class for CentralAgent to hold global static variables

---
+++ everlage/src/de/everlage/ca/core/CAGlobal.java
@@ -0,0 +1,19 @@
+/**
+ *  File: CAGlobal.java    
+ * Created on Jan 14, 2003
+ *
+*/
+package de.everlage.ca.core;
+
+import org.apache.log4j.Logger;
+
+/**
+ * New Class
+ * @author waffel
+ *
+ * 
+ */
+public final class CAGlobal {
+
+	public static Logger log;
+}
