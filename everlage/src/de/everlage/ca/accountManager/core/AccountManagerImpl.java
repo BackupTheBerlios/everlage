d76a3022244d2b81a9405b955b7b5e366aec7506
waffel <waffel>
2003-01-16 13:47:00 +0000

    [feature] Impelementation of the accountManager

---
+++ everlage/src/de/everlage/ca/accountManager/core/AccountManagerImpl.java
@@ -0,0 +1,45 @@
+/**
+ * $ID$
+ * File: AccountManagerImpl.java    Created on Jan 10, 2003
+ *
+*/
+package de.everlage.ca.accountManager.core;
+
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+import de.everlage.ca.accountManager.AccountManagerInt;
+import de.everlage.ca.accountManager.comm.extern.Account;
+import de.everlage.ca.accountManager.exception.extern.InvalidAccountIDException;
+import de.everlage.ca.core.CAGlobal;
+import de.everlage.ca.core.CentralAgent;
+import de.everlage.ca.exception.extern.InternalEVerlageError;
+import de.everlage.ca.exception.extern.InvalidAgentException;
+
+/**
+ * New Class
+ * @author waffel
+ *
+ * 
+ */
+public final class AccountManagerImpl extends UnicastRemoteObject implements AccountManagerInt {
+
+	/**
+	 * Constructor for AccountManagerImpl.
+		 * @throws RemoteException
+		 */
+	public AccountManagerImpl() throws RemoteException {
+		super();
+	}
+
+	/* (non-Javadoc)
+	 * @see de.everlage.ca.accountManager.AccountManagerInt#getAccount(long, long, long, long)
+	 */
+	public Account getAccount(long agentID, long caSessionID, long userID, long accountID)
+		throws RemoteException, InternalEVerlageError, InvalidAgentException, InvalidAccountIDException {
+
+		CAGlobal.log.debug(this);
+		return CentralAgent.l_accountManager.getAccount(userID, accountID);
+	}
+
+}
