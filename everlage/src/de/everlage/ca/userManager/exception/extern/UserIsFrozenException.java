e1971eef612a42a17ce14e9ec767df866424b95b
waffel <waffel>
2003-01-16 13:31:53 +0000

    [feature] Exception thrown when the user is locked

---
+++ everlage/src/de/everlage/ca/userManager/exception/extern/UserIsFrozenException.java
@@ -0,0 +1,18 @@
+/**
+ * $ID$
+ * File:  UserIsFrozenException.java    Created on Jan 10, 2003
+ *
+*/
+package de.everlage.ca.userManager.exception.extern;
+
+import java.io.Serializable;
+
+/**
+ * New Class
+ * @author waffel
+ *
+ * 
+ */
+public class UserIsFrozenException extends Exception implements Serializable {
+
+}
