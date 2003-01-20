97a1d353890e60276688865adc56c9bf0dff60d9
waffel <waffel>
2003-01-20 16:01:04 +0000

    [intern] two methods removed

--- everlage/src/de/everlage/ca/CentralAgentInt.java
+++ everlage/src/de/everlage/ca/CentralAgentInt.java
@@ -1,12 +1,7 @@
 package de.everlage.ca;
 
-import java.sql.Connection;
-
-import de.everlage.ca.exception.extern.InternalEVerlageError;
 
 
 public interface CentralAgentInt {
   
- public Connection getDBConnection() throws InternalEVerlageError;
- public void freeDBConnection(Connection con);
 }
