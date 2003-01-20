6223f17d9e569a492ecc6d47b33d413a2a055779
waffel <waffel>
2003-01-20 16:02:45 +0000

    [intern] static fields for database connection added

--- everlage/src/de/everlage/ca/core/CAGlobal.java
+++ everlage/src/de/everlage/ca/core/CAGlobal.java
@@ -5,6 +5,9 @@
 */
 package de.everlage.ca.core;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.apache.log4j.Logger;
 
 /**
@@ -16,4 +19,18 @@ import org.apache.log4j.Logger;
 public final class CAGlobal {
 
 	public static Logger log;
+
+	public static Map dbDrivers;
+  
+  public static Map dbUrls;
+  
+  static {
+    dbDrivers = new HashMap(2);
+    dbUrls = new HashMap(2);
+    dbDrivers.put("ORACLE", "oracle.jdbc.driver.OracleDriver");
+    dbDrivers.put("POSTGRES", "org.postgresql.Driver");
+    dbUrls.put("ORACLE", "jdbc:oracle:thin:@");
+    dbUrls.put("POSTGRES", "jdbc:postgresql:");
+  }
+
 }
