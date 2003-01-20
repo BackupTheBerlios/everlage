3cb37997d6ff96fc95ad1b620d88d04d6f6e3522
waffel <waffel>
2003-01-20 16:13:01 +0000

    [feature] Communication Class for the ua-login process

---
+++ everlage/src/de/everlage/ca/componentManager/comm/extern/UALoginResult.java
@@ -0,0 +1,20 @@
+/**
+ *  File: UALoginResult.java    
+ * Created on Jan 20, 2003
+ *
+*/
+package de.everlage.ca.componentManager.comm.extern;
+
+import java.io.Serializable;
+
+/**
+ * New Class
+ * @author waffel
+ *
+ * 
+ */
+public class UALoginResult implements Serializable {
+
+	public long userAgentID;
+	public long caSessionID;
+}
