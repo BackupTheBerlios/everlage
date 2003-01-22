d9600ac56fb84a639dd4e0c9668117ee25cd29bf
waffel <waffel>
2003-01-22 16:52:53 +0000

    [feature] Exception for anonymous users added

---
+++ everlage/src/de/everlage/ca/userManager/exception/extern/AnonymousLoginNotPossible.java
@@ -0,0 +1,18 @@
+/**
+ *  File: AnonymousLoginNotPossible.java    
+ * Created on Jan 21, 2003
+ *
+*/
+package de.everlage.ca.userManager.exception.extern;
+
+import java.io.Serializable;
+
+/**
+ * New Class
+ * @author waffel
+ *
+ * 
+ */
+public class AnonymousLoginNotPossible extends Exception implements Serializable {
+
+}
