299456c694beb19b971a3e034dfd560e3b469760
waffel <waffel>
2003-01-22 16:45:32 +0000

    [comment] adding comment

--- everlage/src/de/everlage/ca/core/CAGlobal.java
+++ everlage/src/de/everlage/ca/core/CAGlobal.java
@@ -1,6 +1,6 @@
 /**
- *  File: CAGlobal.java    
- * Created on Jan 14, 2003
+ * $Id: CAGlobal.java,v 1.3 2003/01/22 16:45:32 waffel Exp $  
+ * File: CAGlobal.java    Created on Jan 14, 2003
  *
 */
 package de.everlage.ca.core;
@@ -11,26 +11,35 @@ import java.util.Map;
 import org.apache.log4j.Logger;
 
 /**
- * New Class
+ * Globale Datenklasse, in welcher statische Variablen für alle Manager des CentralAgent zugänglich
+ * sind.
  * @author waffel
- *
  * 
  */
 public final class CAGlobal {
-
+	/**
+	 * Variable, über welche Logausgaben erzeugt werden können.
+	 * @see Logger
+	 */
 	public static Logger log;
-
+	/**
+	 * In dieser Map sind für verschiedene Datenbanksysteme die Driver-Strings vorbelegt. Im Moment
+	 * unterstützte Systeme sind ORACLE und POSTGRES.
+	 */
 	public static Map dbDrivers;
-  
-  public static Map dbUrls;
-  
-  static {
-    dbDrivers = new HashMap(2);
-    dbUrls = new HashMap(2);
-    dbDrivers.put("ORACLE", "oracle.jdbc.driver.OracleDriver");
-    dbDrivers.put("POSTGRES", "org.postgresql.Driver");
-    dbUrls.put("ORACLE", "jdbc:oracle:thin:@");
-    dbUrls.put("POSTGRES", "jdbc:postgresql:");
-  }
+	/**
+	 * In dieser Map sind für verschiedene Datenbanksysteme die URL-Strings vorbelegt, Im Moment
+	 * unterstützte Systeme sind ORACLE und POSTGRES
+	 */
+	public static Map dbUrls;
+
+	static {
+		dbDrivers = new HashMap(2);
+		dbUrls = new HashMap(2);
+		dbDrivers.put("ORACLE", "oracle.jdbc.driver.OracleDriver");
+		dbDrivers.put("POSTGRES", "org.postgresql.Driver");
+		dbUrls.put("ORACLE", "jdbc:oracle:thin:@");
+		dbUrls.put("POSTGRES", "jdbc:postgresql:");
+	}
 
 }
