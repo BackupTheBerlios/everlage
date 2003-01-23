8976b84f386fa125fa51320ccec1de6edd4d8307
waffel <waffel>
2003-01-23 17:20:59 +0000

    [feature] new method createAccountForUser

--- everlage/src/de/everlage/ca/accountManager/core/LocalAccountManager.java
+++ everlage/src/de/everlage/ca/accountManager/core/LocalAccountManager.java
@@ -5,6 +5,11 @@
 */
 package de.everlage.ca.accountManager.core;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import de.everlage.ca.LocalManagerAbs;
 import de.everlage.ca.accountManager.comm.extern.Account;
 import de.everlage.ca.core.CAGlobal;
@@ -30,4 +35,40 @@ public final class LocalAccountManager extends LocalManagerAbs {
 		return new Account();
 	}
 
+	public long createAccountForUser(long userID, double balance, Connection dbConnection)
+		throws InternalEVerlageError {
+		PreparedStatement pstmt = null;
+		ResultSet res = null;
+		try {
+			pstmt = dbConnection.prepareStatement(this.pHandler.getProperty("getNextAccountID", this));
+      res = pstmt.executeQuery();
+      if (!res.next()) {
+        throw new InternalEVerlageError("Cannot create new Account (Sequence Propblem"); 
+      }
+      long accountID = res.getLong(1);
+      res.close();
+      res=null;
+			pstmt =
+				dbConnection.prepareStatement(this.pHandler.getProperty("createAccountForUser", this));
+			pstmt.setLong(1, accountID);
+      pstmt.setLong(2, userID);
+      pstmt.setDouble(3, balance);
+			pstmt.executeUpdate();
+			pstmt.close();
+			pstmt = null;
+      return accountID;
+		} catch (SQLException e) {
+			CAGlobal.log.error(e);
+			throw new InternalEVerlageError(e);
+		} finally {
+      try {
+        if (pstmt != null) {
+          pstmt.close(); pstmt = null;
+        }
+        if (res != null) {
+          res.close(); res = null;
+        }
+      }  catch (Exception e) {}
+		}
+	}
 }
