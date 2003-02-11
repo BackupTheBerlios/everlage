a9633be35a7ecfdea8b830d6f1e6c6f9b7a94e6b
waffel <waffel>
2003-02-11 15:15:06 +0000

    [feature] list of available providerAgents added as a map

--- everlage/src/de/everlage/ca/componentManager/comm/extern/UALoginResult.java
+++ everlage/src/de/everlage/ca/componentManager/comm/extern/UALoginResult.java
@@ -1,11 +1,12 @@
 /**
- * $Id: UALoginResult.java,v 1.2 2003/01/22 16:40:49 waffel Exp $ 
+ * $Id: UALoginResult.java,v 1.3 2003/02/11 15:15:06 waffel Exp $ 
  * File: UALoginResult.java    Created on Jan 20, 2003
  *
 */
 package de.everlage.ca.componentManager.comm.extern;
 
 import java.io.Serializable;
+import java.util.Map;
 
 /**
  * Daten nach einem UA-Login. Hier steht dann die caSessionID und die userAgentID drin, welche dem
@@ -18,4 +19,5 @@ public class UALoginResult implements Serializable {
 
 	public long userAgentID;
 	public long caSessionID;
+  public Map providerAgentList;
 }
--- everlage/src/de/everlage/ca/componentManager/comm/intern/UAData.java
+++ everlage/src/de/everlage/ca/componentManager/comm/intern/UAData.java
@@ -1,9 +1,11 @@
 /**
- * $Id: UAData.java,v 1.2 2003/01/22 16:42:42 waffel Exp $ 
+ * $Id: UAData.java,v 1.3 2003/02/11 15:16:01 waffel Exp $ 
  * File: UAData.java    Created on Jan 20, 2003
  *
 */
 package de.everlage.ca.componentManager.comm.intern;
+
+import java.util.Map;
 
 import de.everlage.ua.UserAgentInt;
 
@@ -19,11 +21,13 @@ public final class UAData {
 	public long uaSessionID;
 	public String uaRMIAddress;
 	public UserAgentInt userAgent;
+  public Map providerAgentList;
 
-	public UAData(long uaID, long sessionID, String rmi, UserAgentInt ua) {
+	public UAData(long uaID, long sessionID, String rmi, UserAgentInt ua, Map paList) {
 		this.userAgentID = uaID;
 		this.uaSessionID = sessionID;
 		this.uaRMIAddress = rmi;
 		this.userAgent = ua;
+    this.providerAgentList = paList;
 	}
 }
