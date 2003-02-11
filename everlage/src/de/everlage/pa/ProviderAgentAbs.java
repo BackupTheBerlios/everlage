205ef46c53789d6ca9a9918c74289d7c0d501c54
waffel <waffel>
2003-02-11 15:22:36 +0000

    [feature] method for title searching added

--- everlage/src/de/everlage/pa/ProviderAgentAbs.java
+++ everlage/src/de/everlage/pa/ProviderAgentAbs.java
@@ -1,5 +1,5 @@
 /**
- * $Id: ProviderAgentAbs.java,v 1.1 2003/01/29 17:32:57 waffel Exp $ 
+ * $Id: ProviderAgentAbs.java,v 1.2 2003/02/11 15:22:36 waffel Exp $ 
  * File: ProviderAgentAbs.java    Created on Jan 29, 2003
  *
 */
@@ -13,6 +13,7 @@ import java.rmi.server.UnicastRemoteObject;
 
 import de.everlage.ca.componentManager.ComponentManagerInt;
 import de.everlage.ca.userManager.UserManagerInt;
+import de.everlage.pa.comm.extern.TitleSearchRes;
 
 /**
  * Abstrakte Klasse für alle ProviderAgents. Hier werden die Schnittstellen zum CentralAgent per RMI
@@ -50,5 +51,12 @@ public class ProviderAgentAbs extends UnicastRemoteObject implements ProviderAge
 		System.out.println("ComponentManager registered");
 		userManager = (UserManagerInt) Naming.lookup(rmiReg + "UserManager");
 		System.out.println("UserManager registered");
+	}
+
+	/* (non-Javadoc)
+	 * @see de.everlage.pa.ProviderAgentInt#searchTitle(java.lang.String)
+	 */
+	public TitleSearchRes searchTitle(String searchStr) throws RemoteException {
+		return null;
 	}
 }
--- everlage/src/de/everlage/pa/ProviderAgentInt.java
+++ everlage/src/de/everlage/pa/ProviderAgentInt.java
@@ -1,11 +1,14 @@
 /**
- * $Id: ProviderAgentInt.java,v 1.1 2003/01/29 17:33:14 waffel Exp $ 
+ * $Id: ProviderAgentInt.java,v 1.2 2003/02/11 15:23:02 waffel Exp $ 
  * File: ProviderAgentInt.java    Created on Jan 29, 2003
  *
 */
 package de.everlage.pa;
 
 import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+import de.everlage.pa.comm.extern.TitleSearchRes;
 
 /**
  * Interface für alle ProviderAgents. Auf dieses Interface wird beim CentralAgent gecastet und die
@@ -15,5 +18,7 @@ import java.rmi.Remote;
  * 
  */
 public interface ProviderAgentInt extends Remote {
+  
+  TitleSearchRes searchTitle(String searchStr) throws RemoteException;
 
 }
