bf8691e4a74b152865fe6ea36fe6f2b01041e67b
waffel <waffel>
2003-03-05 16:48:53 +0000

    [feature] Excpetion if an user alrady logged out

---
+++ everlage/src/de/everlage/ca/userManager/exception/extern/UserAlradyLoggedOutException.java
@@ -0,0 +1,15 @@
+/*
+ * Created on Mar 5, 2003
+ * File UserAlradyLoggedOutException.java
+ * 
+ */
+package de.everlage.ca.userManager.exception.extern;
+
+import java.io.Serializable;
+
+/**
+ * @author waffel
+ */
+public class UserAlradyLoggedOutException extends Exception implements Serializable {
+
+}
