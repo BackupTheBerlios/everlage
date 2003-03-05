8b9bed5d83169c9cc2b5fa5723253763038b0260
waffel <waffel>
2003-03-05 14:04:09 +0000

    [feature] Adding Tests

---
+++ everlage/test/de/everlage/ca/AllTests.java
@@ -0,0 +1,23 @@
+/*
+ * Created on Mar 5, 2003
+ * File AllTests.java
+ * 
+ */
+package de.everlage.ca;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+/**
+ * @author waffel
+ */
+public class AllTests extends de.everlage.AllTests {
+
+	public static Test suite() {
+		TestSuite suite = new TestSuite("Test for de.everlage.ca");
+		//$JUnit-BEGIN$
+    suite.addTest(de.everlage.ca.core.AllTests.suite());
+		//$JUnit-END$
+		return suite;
+	}
+}
