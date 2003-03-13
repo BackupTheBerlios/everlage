6e35ad77e1a6cd4628fcb379f3957f99c6ea2aec
waffel <waffel>
2003-03-13 17:23:13 +0000

    [feature] TestUA now supports SimpleSearch

--- everlage/test/de/everlage/ca/componentManager/TestUA.java
+++ everlage/test/de/everlage/ca/componentManager/TestUA.java
@@ -10,9 +10,12 @@ import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.List;
 import java.util.Map;
 
 import de.everlage.TestGlobal;
+import de.everlage.ca.componentManager.exception.extern.InvalidQueryException;
+import de.everlage.ca.exception.extern.InvalidAgentException;
 import de.everlage.ca.userManager.UserManagerInt;
 import de.everlage.ua.UserAgentInt;
 
@@ -21,8 +24,11 @@ import de.everlage.ua.UserAgentInt;
  */
 public class TestUA extends UnicastRemoteObject implements UserAgentInt {
 
-  private ComponentManagerInt componentManager;
-  private UserManagerInt userManager;
+	private ComponentManagerInt componentManager;
+	private UserManagerInt userManager;
+	private long agentID;
+	private long caSessionID;
+	private List answerList;
 
 	/* (non-Javadoc)
 	 * @see de.everlage.ua.UserAgentInt#init()
@@ -36,15 +42,16 @@ public class TestUA extends UnicastRemoteObject implements UserAgentInt {
 	}
 
 	public void init() throws RemoteException {
-    try {
-		Naming.rebind(TestGlobal.uaRMIAddress, this);
-    setComponentManager((ComponentManagerInt)Naming.lookup(TestGlobal.componentManagerRMI));
-    setUserManager((UserManagerInt)Naming.lookup(TestGlobal.userManagerRMI));
-    } catch (MalformedURLException e) {
-      throw new RemoteException(e.getMessage());
-    } catch (NotBoundException e) {
-      throw new RemoteException(e.getMessage());
-    }
+		try {
+			Naming.rebind(TestGlobal.uaRMIAddress, this);
+			setComponentManager((ComponentManagerInt) Naming.lookup(TestGlobal.componentManagerRMI));
+			setUserManager((UserManagerInt) Naming.lookup(TestGlobal.userManagerRMI));
+      setAnswerList(null);
+		} catch (MalformedURLException e) {
+			throw new RemoteException(e.getMessage());
+		} catch (NotBoundException e) {
+			throw new RemoteException(e.getMessage());
+		}
 
 	}
 
@@ -84,6 +91,71 @@ public class TestUA extends UnicastRemoteObject implements UserAgentInt {
 	 */
 	public void setUserManager(UserManagerInt userManager) {
 		this.userManager = userManager;
+	}
+
+	/* (non-Javadoc)
+	 * @see de.everlage.ua.UserAgentInt#putAnswers(java.util.List)
+	 */
+	public void putAnswers(List answers) throws RemoteException {
+		setAnswerList(answers);
+	}
+
+	/**
+	 * @return long
+	 */
+	public long getAgentID() {
+		return agentID;
+	}
+
+	/**
+	 * @return long
+	 */
+	public long getCaSessionID() {
+		return caSessionID;
+	}
+
+	/**
+	 * Sets the agentID.
+	 * @param agentID The agentID to set
+	 */
+	public void setAgentID(long agentID) {
+		this.agentID = agentID;
+	}
+
+	/**
+	 * Sets the caSessionID.
+	 * @param caSessionID The caSessionID to set
+	 */
+	public void setCaSessionID(long caSessionID) {
+		this.caSessionID = caSessionID;
+	}
+
+	/* (non-Javadoc)
+	 * @see de.everlage.ua.UserAgentInt#search()
+	 */
+	public void search(String searchString) throws RemoteException {
+		try {
+			this.componentManager.sendSearchToAllPAs(this.agentID, this.caSessionID, searchString);
+		} catch (InvalidQueryException e) {
+			e.printStackTrace();
+		} catch (InvalidAgentException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @return List
+	 */
+	public List getAnswerList() {
+		return this.answerList;
+	}
+
+	/**
+	 * Sets the answerList.
+	 * @param answerList The answerList to set
+	 */
+	public void setAnswerList(List answerList) {
+		this.answerList = answerList;
 	}
 
 }
