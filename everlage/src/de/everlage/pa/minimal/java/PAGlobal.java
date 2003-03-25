237c1c92913e1415bb8a8ed531d01307a3bac531
waffel <waffel>
2003-03-25 17:01:41 +0000

    [feature] Global class for the java-pa

---
+++ everlage/src/de/everlage/pa/minimal/java/PAGlobal.java
@@ -0,0 +1,17 @@
+/*
+ * Created on Mar 21, 2003
+ * File PAGlobal.java
+ * 
+ */
+package de.everlage.pa.minimal.java;
+
+import de.everlage.ca.componentManager.comm.extern.PALoginResult;
+
+/**
+ * @author waffel
+ */
+public class PAGlobal {
+
+  public static PALoginResult paData;
+
+}
