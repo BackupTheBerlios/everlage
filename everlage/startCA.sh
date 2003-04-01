09f8621351ce61d00fefc1a92cbd5ff0a8aa3517
waffel <waffel>
2003-04-01 14:07:28 +0000

    [intern] classpath expanded

--- everlage/startCA.sh
+++ everlage/startCA.sh
@@ -1,9 +1,18 @@
 #!/bin/sh
 
-export CLASSPATH=./bin/:$CLASSPATH
+export CLASSPATH=./bin/
 export CLASSPATH=./bin/ext/log4j.jar:$CLASSPATH
 export CLASSPATH=./bin/ext/postgresjdbc7.1-1.2.jar:$CLASSPATH
 export CLASSPATH=./test-out:$CLASSPATH
+export CLASSPATH=./bin/ext/Tidy.jar:$CLASSPATH
+export CLASSPATH=./bin/ext/httpunit.jar:$CLASSPATH
+export CLASSPATH=./bin/ext/js.jar:$CLASSPATH
+export CLASSPATH=./bin/ext/nekohtml.jar:$CLASSPATH
+export CLASSPATH=./bin/ext/xercesImpl.jar:$CLASSPATH
+export CLASSPATH=./bin/ext/xmlParserAPIs.jar:$CLASSPATH
+export CLASSPATH=./bin/ext/exml.jar:$CLASSPATH
+export CLASSPATH=./bin/ext/dtdparser.jar:$CLASSPATH
+
 
 /usr/bin/nohup rmiregistry &
 echo $! > rmi.pid
